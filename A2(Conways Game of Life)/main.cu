d001f1b92fd1ec5af6790f55b8e7e6af67579cf6
ConnorSpackman <connorspackman@ubuntu.lan>
2024-10-10 11:05:04 -0700

    Fixed

--- A2(Conways Game of Life)/main.cu
+++ A2(Conways Game of Life)/main.cu
@@ -16,40 +16,75 @@ int *init_grid(int width, int height)
     return grid;
 }
 
-// __global__ void calc_new_grid(int *A, int *B,int numARows, int numAColumns,int numBRows, int numBColumns,)
-// {
-// 	// If Corner 
-//         //If Top Left
-//         // If Top Right
-//         //If Bottom Left
-//         //If Bottom Right 
-//     //If Ghost Row
-//         //Top
-//         //Bottom
-//     //If Ghost Col
-//         //Right 
-//         //Left
-// }
+__global__ void calc_new_grid(int *A,int *output,int numRows, int numColumns)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int i = row*numRows+col;
+    printf("Hello from gpuo");
+    // int up = (row-1)*numRows+col;
+    // int down = (row+1)*numRows+col;
+    // int left = row*numRows+(col-1);
+    // int right = row*numRows+(col+1);
+    // int count=0;
+
+    // if (up<0)
+    // {
+    //     up = (numRows)*numRows+col;
+    // }
+    // if (down>numRows)
+    // {
+    //    down = (0)*numRows+col;
+    // }
+    // if(right>numColumns){
+    //     right = (col+1);
+    // }
+    // if(left<0){
+    //     left = row*numRows;
+    // }
+
+    // if(A[up]){
+    //     count++;
+    // }
+    // if(A[down]){
+    //     count++;
+    // }
+    // if(A[right]){
+    //     count++;
+    // }
+    // if(A[left]){
+    //     count++;
+    // }
+    output[i]=1; 
+    // if(A[i]&&count>2){
+    //     output[i]=1;
+    // }
+    // if(!A[i]&&count==3){
+    //     output[i]=1;
+    // }
+    // if (count<2 && count>3)
+    // {
+    //     output[i]=0;
+    // }
+
+}
+
 
 void print_grid(int *grid, int width, int height)
 {
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < height; j++)
+        // Print visual representation
+        for (int j = 0; j < width; j++)
         {
-           
-            if (grid[i+j])
-            {
-                printf("\u2588");
-                // printf("1");
-            }
-            if (!grid[i+j])
-            {
-                printf("  ");
-                // printf("0");
-            }
-            
-            // printf("%d", grid[i*j]);
+            printf(grid[i * width + j] ? "█" : " ");
+        }
+        printf("    "); // Add some space between representations
+        
+        // Print 1s and 0s
+        for (int j = 0; j < width; j++)
+        {
+            printf("%d ", grid[i * width + j]);
         }
         printf("\n");
     }
@@ -57,19 +92,52 @@ void print_grid(int *grid, int width, int height)
 
 int main(int argc, char **argv)
 {
-    int GRIDWITH = 5;
-    int GRIDHEIGHT = 5;
+    int GRIDWITH = 25;
+    int GRIDHEIGHT = 25;
     // 8 Digit seed
-    srand(92006191);
+    // srand(92007191);
 
     int *grid = init_grid(GRIDWITH, GRIDHEIGHT);
+    int *output = new int[GRIDWITH*GRIDHEIGHT];
 
-    for (size_t i = 0; i < (GRIDWITH * GRIDHEIGHT); i++)
-    {
-        printf("%d", grid[i]);
-    }
+
+    	// device variables
+	int *deviceA;
+	int *deviceB;
+	// float *deviceC;
+
+    unsigned int size_A = GRIDWITH * GRIDHEIGHT;
+	unsigned int mem_size_A = sizeof(int) * size_A;
+	unsigned int size_B = GRIDWITH * GRIDHEIGHT;
+	unsigned int mem_size_B = sizeof(int) * size_B;
+
+    cudaMalloc((void **)&deviceA, mem_size_A);
+	cudaMalloc((void **)&deviceB, mem_size_B);
+
+    cudaMemcpy(deviceA, grid, mem_size_A, cudaMemcpyHostToDevice);
+    // cudaMemcpy(deviceB, output, mem_size_B, cudaMemcpyHostToDevice);
+
+
+
+	// dim3 threads(GRIDWITH, GRIDHEIGHT, 1);
+	// dim3 grid(((1 - 1) / threads.x) + 1, ((1 - 1) / threads.y) + 1, 1);
+    calc_new_grid<<<(1,1),(1,1)>>>(deviceA,deviceB,GRIDWITH,GRIDHEIGHT);
+    cudaMemcpy(output, deviceB, mem_size_A, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    // for (size_t i = 0; i < (GRIDWITH * GRIDHEIGHT); i++)
+    // {
+    //     printf("%d", grid[i]);
+    // }
+    print_grid(grid,GRIDHEIGHT,GRIDWITH);
     printf("\n \n");
-    print_grid(grid,GRIDWITH,GRIDHEIGHT);
+    // for (size_t i = 0; i < (GRIDWITH * GRIDHEIGHT); i++)
+    // {
+    //     printf("%d", output[i]);
+    // }
+    print_grid(output,GRIDHEIGHT,GRIDWITH);
+    printf("\n \n");
+    // print_grid(grid,GRIDWITH,GRIDHEIGHT);
+    // print_grid(output,GRIDWITH,GRIDHEIGHT);
 
     return 1;
 }
