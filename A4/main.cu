f99a4bd08641094ec7b16210595cd2c97295a4e2
ConnorSpackman <connorspackman@ubuntu.lan>
2024-10-29 10:39:27 -0700

    gpu convultion implementation

--- A4/main.cu
+++ A4/main.cu
@@ -5,10 +5,9 @@
 #include <cuda.h>
 #include <assert.h>
 
-
 void cpu_convultion(int *img, int *kernel, int *imgf, int Nx, int Ny, int kernal_size)
 {
-    int center = kernel[(kernal_size / 2)];
+    int center = (kernal_size - 1) / 2;
     int sum, ii, jj;
     for (int i = center; i < (Ny - center); i++)
     {
@@ -30,9 +29,35 @@ void cpu_convultion(int *img, int *kernel, int *imgf, int Nx, int Ny, int kernal
 }
 // test
 
-__global__ void gpu_convultion(int *img, int *kernel, int *imgf, int Nx, int Ny, int kernal_size){
+__global__ void gpu_convultion(int *img, int *kernel, int *imgf, int Nx, int Ny, int kernal_size)
+{
     // Implement the GPU convolution kernel here
+    int center = (kernal_size - 1) / 2;
+    int iy = blockIdx.x + (kernal_size - 1) / 2;
+    int ix = threadIdx.x + (kernal_size - 1) / 2;
+    int idx = iy * Nx + ix;
+    int K2 = kernal_size * kernal_size;
+    extern __shared__ int shared_img[];
 
+    if (threadIdx.x < K2)
+    {
+        shared_img[threadIdx.x] = kernal_size[threadIdx.x];
+        __syncthreads();
+    }
+    if (idx < Nx * Ny)
+    {
+        int sum = 0;
+        for (int ki = 0; ki < kernal_size; ki++)
+        {
+            for (int kj = 0; kj < kernal_size; kj++)
+            {
+                int ii = ix + kj - center;
+                int jj = iy + ki - center;
+                sum += img[jj * Nx + ii] * shared_img[ki * kernal_size + kj];
+            }
+            imgf[idx] = sum;
+        }
+    }
 }
 
 int *init_grid(int width, int height)
@@ -60,12 +85,12 @@ __global__ void check(int *a, int *b, int width, int height)
 
 int main()
 {
-    //Starting parameters
+    // Starting parameters
     int width = 10;
     int height = 10;
     srand(92507191);
 
-    //Init random Grid
+    // Init random Grid
     int *gpu_grid, *cpu_grid;
     gpu_grid = init_grid(width, height);
     cpu_grid = gpu_grid;
@@ -74,11 +99,11 @@ int main()
     int *kernel = new int[kernal_size * kernal_size];
 
     int *cpu_kernal_out = new int[(width) * (height)];
-    
+
     cpu_convultion(cpu_grid, kernel, cpu_kernal_out, width, height, kernal_size);
     gpu_grid = cpu_kernal_out;
-    //Check CPU_GRID and GPU_GRID
-    unsigned int mem_size_A = sizeof(int) * (width*height);
+    // Check CPU_GRID and GPU_GRID
+    unsigned int mem_size_A = sizeof(int) * (width * height);
     int *space_gpu_grid, *space_cpu_grid;
 
     cudaMalloc((void **)&space_gpu_grid, mem_size_A);
@@ -89,9 +114,6 @@ int main()
     // Number of blocks(vector) and number of threads per block(vector)
     check<<<(1), (height, width)>>>(space_gpu_grid, space_cpu_grid, width, height);
     cudaDeviceSynchronize();
-
-
-
 
     printf("Done\n");
     return 1;
